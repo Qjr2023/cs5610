2853aaa3edb1f118bf3a6b48358b0427bafdf50e
Qjr2023 <liufang1566@gmail.com>
2025-02-06 11:56:18 -0800

    Refactor index.html and main.js to update shopping list functionality and comment out unused code

--- week5/js/main.js
+++ week5/js/main.js
@@ -1,22 +1,43 @@
-const radiusP = document.querySelector('#radius');
-const resultP = document.querySelector('#result');
+// const radiusP = document.querySelector('#radius');
+// const resultP = document.querySelector('#result');
 
-function getNumber() {
-    let radius = prompt("Please enter a number for a circle radius:");
-    radiusP.innerText += radius;
-    return radius;
-}
+// function getNumber() {
+//     let radius = prompt("Please enter a number for a circle radius:");
+//     radiusP.innerText += radius;
+//     return radius;
+// }
 
-function calculateArea() {
-    const number = getNumber();
-    console.log(number);
-    // check if the the number is numeric or not
-    if(isNaN(number)) {
-        return "Error: You entered an invalid number";
-    } else {
-        return (Math.PI * Math.pow(number, 2)).toFixed(2);
+// function calculateArea() {
+//     const number = getNumber();
+//     console.log(number);
+//     // check if the the number is numeric or not
+//     if(isNaN(number)) {
+//         return "Error: You entered an invalid number";
+//     } else {
+//         return (Math.PI * Math.pow(number, 2)).toFixed(2);
+//     }
+// }
+
+// const result = calculateArea();
+// resultP.innerText += result;
+
+
+
+let shoppingItems = ["bread", "cheese", "green pepper"];
+// ul class = "shopping-list"
+const shoppingListElement = document.querySelector('.shopping');
+
+function populateSgoppingList(shoppingListItems) {
+
+    for (let i = 0; i < shoppingListItems.length; i++) {
+        // we need an element to each question
+        const li = document.createElement('li');
+        li.innerText = shoppingListItems[i];
+        shoppingListElement.appendChild(li);
     }
+    // shoppingItems.forEach(item => {
+    //     console.log(item);
 }
+populateSgoppingList(shoppingItems);
 
-const result = calculateArea();
-resultP.innerText += result;
+
