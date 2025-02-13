c39a7476873aa789cd8c143725c200445582e277
Qjr2023 <liufang1566@gmail.com>
2025-02-13 11:52:36 -0800

    Refactor app.js to remove file writing logic and add logger functionality in logger.js

--- week6/app.js
+++ week6/app.js
@@ -1,19 +1,21 @@
 // use write file to write the data to the file
-const fs = require('fs');
-fs.writeFile("data.text", "Hello, this is a test", (err) => {
-    if (err) {
-        console.log("File failed")
-    }
-    else {
-        console.log("File written")
-        fs.readFile("data.text", "utf8", (err, data) => {
-            if (err) {
-                console.log("File failed")
-            }
-            else {
-                console.log(data)
-            }
-        });
-    }
-}); 
+// const fs = require('fs');
+// fs.writeFile("data.text", "Hello, this is a test", (err) => {
+//     if (err) {
+//         console.log("File failed")
+//     }
+//     else {
+//         console.log("File written")
+//         fs.readFile("data.text", "utf8", (err, data) => {
+//             if (err) {
+//                 console.log("File failed")
+//             }
+//             else {
+//                 console.log(data)
+//             }
+//         });
+//     }
+// }); 
 
+const logger = require('./logger.js');
+logger.log();
