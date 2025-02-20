1445cb323ac85c99f418b39d2bf943303ed9d229
Qjr2023 <liufang1566@gmail.com>
2025-02-20 12:45:06 -0800

    Refactor task routes to use async/await and enhance task detail view with completion status, passing title and completed to the template

--- week6/routes/tasks.js
+++ week6/routes/tasks.js
@@ -2,22 +2,37 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
-router.get('/', (req, res) => {
-    const promise = axios.get('https://jsonplaceholder.typicode.com/todos/')
-    // res.send('<h1>List of all the tasks</h1>');
-    // console.log(promise);
-    promise
-    .then((response) => {
-        // console.log(response.data);
-        // res.render('tasks', {tasks: response.data});
+router.get('/', async(req, res) => {
+    try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/todos/');
         res.json(response.data);
-    }).catch((err) => console.log(err.status));
+    } catch (err) {
+        console.log(err.status);
+    }
+    
+
+    // const promise = axios.get('https://jsonplaceholder.typicode.com/todos/')
+    // // res.send('<h1>List of all the tasks</h1>');
+    // // console.log(promise);
+    // promise
+    // .then((response) => {
+    //     // console.log(response.data);
+    //     // res.render('tasks', {tasks: response.data});
+    //     res.json(response.data);
+    // }).catch((err) => console.log(err.status));
 
 });
 
-router.get('/:taskId', (req, res) => {
-    console.log(req.params.taskId);
-    res.render('task', {id: req.params.taskId});
+router.get('/:taskId', async(req, res) => {
+    try {
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/todos/${req.params.taskId}`);
+        // res.json(response.data);
+        res.render('task', {id: req.params.taskId, title:response.data.title, completed: response.data.completed});
+    } catch (err) {
+        console.log(err.status);
+    }
+
+    // console.log(req.params.taskId);
     // res.send(`<p>You are reviewing task ${req.params.taskId}</p>`)
 });
 
