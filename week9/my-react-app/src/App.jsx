1abb1713366edff8ef63598a3ec938c4179a8849
Qjr2023 <liufang1566@gmail.com>
2025-03-13 11:33:34 -0700

    Add index.html and update App component with app name

--- week9/my-react-app/src/App.jsx
+++ week9/my-react-app/src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
 export default function App() {
+  const appName = "My React App";
   return (
     <div>
-      <h1>Welcome</h1>
+      <h1>Welcome to {appName}</h1>
     </div>
   );
 }
