4088038766103c4a397af70c030a95a9f931af83
Qjr2023 <liufang1566@gmail.com>
2025-03-13 12:45:28 -0700

    Remove commented-out code in TasksList component for cleaner implementation

--- week9/my-react-app/src/components/TasksList.jsx
+++ week9/my-react-app/src/components/TasksList.jsx
@@ -21,7 +21,8 @@ export default function TasksList() {
             },
         ]);
     
-
+    // setTasks([]);
+    // console.log(tasks);
     return (
         <ul>
             {tasks.map((task) => {
