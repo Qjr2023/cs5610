b244e8270cbdc74d9b56cd35e97e8a24fdff5856
Qjr2023 <liufang1566@gmail.com>
2025-03-13 12:17:44 -0700

    Add TasksList component and integrate it into App component

--- week9/my-react-app/src/App.jsx
+++ week9/my-react-app/src/App.jsx
@@ -1,35 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
+import TasksList from "./components/TasksList";
 
 export default function App() {
-  const tasks = [
-    {
-      id: 1,
-      title: "Review week 9 material",
-      date: "June 4th at 1 pm",
-    },
-    {
-      id: 2,
-      title: "Do quiz 9",
-      date: "June 4th at 6 pm",
-    },
-    {
-      id: 3,
-      title: "Work on assignment 2",
-      date: "June 5th at 8 am",
-    },
-  ];
+  
   const appName = "My React App";
   return (
     <div className="appContainer">
       <Header myAppName={appName} version={2} />
-      <ul>
-        {tasks.map((task) => (
-          <li key={task.id}>
-            <h2>{task.title}</h2>
-          </li>
-        ))}
-      </ul>
+      <TasksList />
     </div>
   );
 }
