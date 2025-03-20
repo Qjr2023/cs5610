dd96dd09c41eee862845fa8baef7fa729f87f253
Qjr2023 <liufang1566@gmail.com>
2025-03-20 11:48:59 -0700

    Add AddTask component and integrate it into App; update styles for form controls

--- week9/my-react-app/src/App.jsx
+++ week9/my-react-app/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./components/Header";
+import AddTask from "./components/AddTask";
 import TasksList from "./components/TasksList";
 
 export default function App() {
@@ -8,6 +9,7 @@ export default function App() {
   return (
     <div className="appContainer">
       <Header myAppName={appName} version={2} />
+      <AddTask />
       <TasksList />
     </div>
   );
