e2de772e77788167cb7bed4fb3210a728131c2aa
Qjr2023 <liufang1566@gmail.com>
2025-03-20 11:56:57 -0700

    Implement form submission in AddTask component; reset input fields after submission

--- week9/my-react-app/src/components/AddTask.jsx
+++ week9/my-react-app/src/components/AddTask.jsx
@@ -3,8 +3,18 @@ import React, {  useState } from 'react'
 export default function AddTask() {
     const [title, setTitle] = useState("");
     const [date, setDate] = useState("");
+    function submitHandler(e){
+        e.preventDefault();
+        const newTask = {
+            title: title,
+            date: date
+        }
+        setTitle("");
+        setDate("");
+        console.log(newTask);
+    }
     return (
-        <form>
+        <form onSubmit={submitHandler}>
         <div className="form-control">
                 <label>Title</label>
                 <input type="text" value={title} onChange={function(e){
