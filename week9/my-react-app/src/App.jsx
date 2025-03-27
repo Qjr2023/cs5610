38dcfed7a1b38117671881082298f4549862f924
Qjr2023 <liufang1566@gmail.com>
2025-03-27 11:46:35 -0700

    Add react-router-dom for routing; update App component with navigation and routes

--- week9/my-react-app/src/App.jsx
+++ week9/my-react-app/src/App.jsx
@@ -2,14 +2,18 @@ import React, { use, useEffect, useState } from "react";
 import Header from "./components/Header";
 import AddTask from "./components/AddTask";
 import TasksList from "./components/TasksList";
+import { Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 export default function App() {
   const appName = "My React App";
   const [tasksFromServer, setTasksFromServer] = useState([]);
   const [showAddTask, setShowAddTask] = useState(false);
 
+
   async function fetchDate() {
     try{
+      // const response = await fetch("http://localhost:5001/tasks", { signle });
       const response = await fetch("http://localhost:5001/tasks");
       if (response.ok) {
         const data = await response.json();
@@ -32,14 +36,28 @@ export default function App() {
 
   return (
     <div className="appContainer">
-      <Header 
-      myAppName={appName} 
-      version={2}
-      onToggleAddTask={toggleShowAddTask}
-      showAddTask={showAddTask}
-       />
-      {showAddTask && <AddTask />}
-      <TasksList tasks={tasksFromServer}/>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/tasks">Tasks</Link>
+      </nav>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Header 
+                myAppName={appName} 
+                version={2}
+                onToggleAddTask={toggleShowAddTask}
+                showAddTask={showAddTask}
+              />
+              {showAddTask && <AddTask />}
+            </>
+          }
+        />
+        <Route path="/tasks" element={<TasksList tasks={tasksFromServer} />} />
+      </Routes>
+
     </div>
   );
 }
